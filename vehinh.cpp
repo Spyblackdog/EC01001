86983debed5c57f79f0c172470f4b1f6c029bc30
Spyblackdog <146566259+Spyblackdog@users.noreply.github.com>
2024-01-18 11:21:12 +0700

    Update vehinh.cpp

--- vehinh.cpp
+++ vehinh.cpp
@@ -1,29 +1,65 @@
-#include <stdio.h>
+						***** 1 *****
+// Vẽ Hình thoi ~
+// #include <stdio.h>
 
-int main()
-{
-	int n;
-	scanf("%d",&n);
-	for (int i=0;i<n;i++)
-	{
-		for (int j=1;j<=n*2;j++)
-		{
-			if (j<=n-i || j>=n+i+1)
-				printf("*");
-			else
-				printf("~");
-		}
-		printf("\n");
-	}
-	for (int i=n-2;i>=0;i--)
-	{
-		for (int j=1;j<=n*2;j++)
-		{
-			if (j<=n-i || j>=n+i+1)
-				printf("*");
-			else
-				printf("~");
-		}
-		printf("\n");
-	}
-}
+// int main()
+// {
+// 	int n;
+// 	scanf("%d",&n);
+// 	for (int i=0;i<n;i++)
+// 	{
+// 		for (int j=1;j<=n*2;j++)
+// 		{
+// 			if (j<=n-i || j>=n+i+1)
+// 				printf("*");
+// 			else
+// 				printf("~");
+// 		}
+// 		printf("\n");
+// 	}
+// 	for (int i=n-2;i>=0;i--)
+// 	{
+// 		for (int j=1;j<=n*2;j++)
+// 		{
+// 			if (j<=n-i || j>=n+i+1)
+// 				printf("*");
+// 			else
+// 				printf("~");
+// 		}
+// 		printf("\n");
+// 	}
+// }
+
+***** 2 *****
+// Vẽ Mũi Tên
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int n;
+// 	scanf("%d",&n);
+// 	for (int i=1;i<=n;i++)
+// 	{
+// 		for (int j=1;j<=2*(i-1);j++)
+// 		{
+// 			printf("~");
+// 		}
+// 		for (int j=1;j<=n-i+1;j++)
+// 		{
+// 			printf("*");
+// 		}
+// 		printf("\n");
+// 	}
+// 	for (int i=n-1;i>=1;i--)
+// 	{
+// 		for (int j=1;j<=2*(i-1);j++)
+// 		{
+// 			printf("~");
+// 		}
+// 		for (int j=1;j<=n-i+1;j++)
+// 		{
+// 			printf("*");
+// 		}
+// 		printf("\n");
+// 	}
+// }
