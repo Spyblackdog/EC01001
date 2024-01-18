8ca8ea0dc069e93141cd6c65a83527fe4b879952
Spyblackdog <146566259+Spyblackdog@users.noreply.github.com>
2024-01-18 15:48:02 +0700

    Update vehinh.cpp

--- vehinh.cpp
+++ vehinh.cpp
@@ -63,3 +63,37 @@
 // 		printf("\n");
 // 	}
 // }
+							**** 3 ****
+Vẽ hình mũi tên 2
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	for (int i=1;i<=n;i++)
+//	{
+//		for (int j=1;j<=(n-i);j++)
+//		{
+//				printf("~");
+//		}
+//		for (int j=n-i+1;j>=1;j--)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (int i=1;i<n;i++)
+//	{
+//		for (int j=1;j<=i;j++)
+//		{
+//			printf("~");
+//		}
+//		for (int j=1;j<=i+1;j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
